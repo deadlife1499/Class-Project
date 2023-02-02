1a101146d5a8de263c7a0115f641f4fbfa0fe844
deadlife1499 <96661238+deadlife1499@users.noreply.github.com>
2023-02-02 08:02:26 -0600

    hdrhdrhdryhdrgdrgdr5ndrmydr6ymndrumn

--- Crash Test Dummy/Assets/Scripts/BasicVehControls.cs
+++ Crash Test Dummy/Assets/Scripts/BasicVehControls.cs
@@ -6,31 +6,31 @@ public class BasicVehControls : MonoBehaviour {
 	[Space(20)]
 	//[Header("Camera")]
 
+	public bool doDynamicFOV = false;
+	public Camera camera;
 	[Range(0, 119)]
-	public int minFOV = 55;
-	[Range(0, minFOV)]
-	public int maxFOV = 90;
+	public float minFOV = 55;
+	[Range(1, 120)]
+	public float maxFOV = 90;
 
     //[Header("Car Setup")]
     [Space(10)]
     [Range(20, 190)]
-    public int maxSpeed = 90; //The maximum speed that the car can reach in km/h.
+    public int maxSpeed = 90;
     [Range(10, 120)]
-    public int maxReverseSpeed = 45; //The maximum speed that the car can reach while going on reverse in km/h.
+    public int maxReverseSpeed = 45;
     [Range(1, 10)]
-    public int accelerationMultiplier = 2; // How fast the car can accelerate. 1 is a slow acceleration and 10 is the fastest.
+    public int accelerationMultiplier = 2; 
     [Space(10)]
     [Range(10, 45)]
-    public int maxSteeringAngle = 27; // The maximum angle that the tires can reach while rotating the steering wheel.
+    public int maxSteeringAngle = 27; 
     [Range(0.1f, 1f)]
-    public float steeringSpeed = 0.5f; // How fast the steering wheel turns.
+    public float steeringSpeed = 0.5f;
     [Space(10)]
     [Range(100, 600)]
-    public int brakeForce = 350; // The strength of the wheel brakes.
+    public int brakeForce = 350;
     [Range(1, 10)]
-    public int decelerationMultiplier = 2; // How fast the car decelerates when the user is not using the throttle.
-    [Range(1, 10)]
-    public int handbrakeDriftMultiplier = 5; // How much grip the car loses when the user hit the handbrake.
+    public int decelerationMultiplier = 2;
     [Space(10)]
     public Vector3 bodyMassCenter;
 
@@ -50,7 +50,7 @@ public class BasicVehControls : MonoBehaviour {
     public WheelCollider rearRightCollider;
 
 	[Space(20)]
-      //[Header("EFFECTS")]
+    //[Header("EFFECTS")]
     [Space(10)]
     public bool useEffects = false;
     public ParticleSystem RLWParticleSystem;
@@ -68,21 +68,18 @@ public class BasicVehControls : MonoBehaviour {
     public AudioSource tireScreechSound;
     float initialCarEngineSoundPitch;
 
-	float carSpeed;
-    bool isDrifting;
-    bool isTractionLocked;
+	public float carSpeed;
 
 	Rigidbody rb;
     float steeringAxis;
     float throttleAxis;
-    float driftingAxis;
     float localVelocityZ;
-    float localVelocityX;
     bool deceleratingCar;
 
 	void Start() {
-		carRigidbody = gameObject.GetComponent<Rigidbody>();
-      	carRigidbody.centerOfMass = bodyMassCenter;
+		rb = gameObject.GetComponent<Rigidbody>();
+      	rb.centerOfMass = bodyMassCenter;
+		camera.fieldOfView = minFOV;
 
 		if(carEngineSound != null){
           	initialCarEngineSoundPitch = carEngineSound.pitch;
@@ -103,7 +100,7 @@ public class BasicVehControls : MonoBehaviour {
 
 	void Update() {
 		carSpeed = (2 * Mathf.PI * frontLeftCollider.radius * frontLeftCollider.rpm * 60) / 1000;
-      	localVelocityZ = transform.InverseTransformDirection(carRigidbody.velocity).z;
+      	localVelocityZ = transform.InverseTransformDirection(rb.velocity).z;
 
         if(Input.GetKey(KeyCode.W)){
           	CancelInvoke("DecelerateCar");
@@ -134,18 +131,31 @@ public class BasicVehControls : MonoBehaviour {
         }
 
 		AnimateWheelMeshes();
+		if(doDynamicFOV && maxFOV > minFOV) {
+			SetFOV();
+		}
 	}
 
-	public void CarSounds(){
+	void SetFOV() {
+		float oldFOV = camera.fieldOfView;
+		float multiplier = carSpeed * 20f / maxSpeed;
+		float diffFOV = maxFOV - minFOV;
+		float newFOV = multiplier * diffFOV + minFOV;
+
+		if(newFOV > oldFOV) {
+			camera.fieldOfView += .04f;
+		}
+		else if(newFOV < oldFOV) {
+			camera.fieldOfView -= .1f;
+		}
+	}
+
+	void CarSounds(){
       	if(useSounds){
-        	try{
-          		if(carEngineSound != null){
-            		float engineSoundPitch = initialCarEngineSoundPitch + (Mathf.Abs(carRigidbody.velocity.magnitude) / 25f);
-            		carEngineSound.pitch = engineSoundPitch;
-          		}
-        	}catch(Exception ex){
-          		Debug.LogWarning(ex);
-        	}
+          	if(carEngineSound != null){
+            	float engineSoundPitch = initialCarEngineSoundPitch + (Mathf.Abs(rb.velocity.magnitude) / 25f);
+            	carEngineSound.pitch = engineSoundPitch;
+          	}
       	}else if(!useSounds){
         	if(carEngineSound != null && carEngineSound.isPlaying){
           		carEngineSound.Stop();
@@ -153,7 +163,7 @@ public class BasicVehControls : MonoBehaviour {
       	}
     }
 
-	public void TurnLeft(){
+	void TurnLeft(){
       	steeringAxis = steeringAxis - (Time.deltaTime * 10f * steeringSpeed);
       	if(steeringAxis < -1f){
         	steeringAxis = -1f;
@@ -164,7 +174,7 @@ public class BasicVehControls : MonoBehaviour {
       	frontRightCollider.steerAngle = Mathf.Lerp(frontRightCollider.steerAngle, steeringAngle, steeringSpeed);
     }
 
-	public void TurnRight(){
+	void TurnRight(){
       	steeringAxis = steeringAxis + (Time.deltaTime * 10f * steeringSpeed);
       	if(steeringAxis > 1f){
         	steeringAxis = 1f;
@@ -174,7 +184,7 @@ public class BasicVehControls : MonoBehaviour {
       	frontRightCollider.steerAngle = Mathf.Lerp(frontRightCollider.steerAngle, steeringAngle, steeringSpeed);
     }
 
-	public void ResetSteeringAngle(){
+	void ResetSteeringAngle(){
       	if(steeringAxis < 0f){
         	steeringAxis = steeringAxis + (Time.deltaTime * 10f * steeringSpeed);
       	}else if(steeringAxis > 0f){
@@ -190,32 +200,118 @@ public class BasicVehControls : MonoBehaviour {
     }
 
 	void AnimateWheelMeshes(){
-      	try{
-        	Quaternion FLWRotation;
-        	Vector3 FLWPosition;
-        	frontLeftCollider.GetWorldPose(out FLWPosition, out FLWRotation);
-        	frontLeftMesh.transform.position = FLWPosition;
-        	frontLeftMesh.transform.rotation = FLWRotation;
+        Quaternion FLWRotation;
+        Vector3 FLWPosition;
+        frontLeftCollider.GetWorldPose(out FLWPosition, out FLWRotation);
+        frontLeftMesh.transform.position = FLWPosition;
+        frontLeftMesh.transform.rotation = FLWRotation;
 
-        	Quaternion FRWRotation;
-        	Vector3 FRWPosition;
-        	frontRightCollider.GetWorldPose(out FRWPosition, out FRWRotation);
-        	frontRightMesh.transform.position = FRWPosition;
-        	frontRightMesh.transform.rotation = FRWRotation;
+        Quaternion FRWRotation;
+        Vector3 FRWPosition;
+        frontRightCollider.GetWorldPose(out FRWPosition, out FRWRotation);
+        frontRightMesh.transform.position = FRWPosition;
+        frontRightMesh.transform.rotation = FRWRotation;
 
-        	Quaternion RLWRotation;
-        	Vector3 RLWPosition;
-        	rearLeftCollider.GetWorldPose(out RLWPosition, out RLWRotation);
-        	rearLeftMesh.transform.position = RLWPosition;
-        	rearLeftMesh.transform.rotation = RLWRotation;
+        Quaternion RLWRotation;
+        Vector3 RLWPosition;
+        rearLeftCollider.GetWorldPose(out RLWPosition, out RLWRotation);
+        rearLeftMesh.transform.position = RLWPosition;
+        rearLeftMesh.transform.rotation = RLWRotation;
 
-        	Quaternion RRWRotation;
-        	Vector3 RRWPosition;
-        	rearRightCollider.GetWorldPose(out RRWPosition, out RRWRotation);
-        	rearRightMesh.transform.position = RRWPosition;
-        	rearRightMesh.transform.rotation = RRWRotation;
-      	}catch(Exception ex){
-        	Debug.LogWarning(ex);
+        Quaternion RRWRotation;
+        Vector3 RRWPosition;
+        rearRightCollider.GetWorldPose(out RRWPosition, out RRWRotation);
+        rearRightMesh.transform.position = RRWPosition;
+        rearRightMesh.transform.rotation = RRWRotation;
+    }
+
+	void GoForward(){
+      	throttleAxis = throttleAxis + (Time.deltaTime * 3f);
+      	if(throttleAxis > 1f){
+        	throttleAxis = 1f;
       	}
+
+      	if(localVelocityZ < -1f){
+        	Brakes();
+      	}else{
+        	if(Mathf.RoundToInt(carSpeed) < maxSpeed){
+          		frontLeftCollider.brakeTorque = 0;
+          		frontLeftCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		frontRightCollider.brakeTorque = 0;
+          		frontRightCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		rearLeftCollider.brakeTorque = 0;
+          		rearLeftCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		rearRightCollider.brakeTorque = 0;
+          		rearRightCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+        	}else {
+    			frontLeftCollider.motorTorque = 0;
+    			frontRightCollider.motorTorque = 0;
+          		rearLeftCollider.motorTorque = 0;
+    			rearRightCollider.motorTorque = 0;
+    		}
+      	}
+    }
+
+	void GoReverse(){
+      	throttleAxis = throttleAxis - (Time.deltaTime * 3f);
+      	if(throttleAxis < -1f){
+        	throttleAxis = -1f;
+      	}
+
+      	if(localVelocityZ > 1f){
+        	Brakes();
+      	}else{
+        	if(Mathf.Abs(Mathf.RoundToInt(carSpeed)) < maxReverseSpeed){
+          		frontLeftCollider.brakeTorque = 0;
+          		frontLeftCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		frontRightCollider.brakeTorque = 0;
+          		frontRightCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		rearLeftCollider.brakeTorque = 0;
+          		rearLeftCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+          		rearRightCollider.brakeTorque = 0;
+          		rearRightCollider.motorTorque = (accelerationMultiplier * 50f) * throttleAxis;
+        	}else {
+    			frontLeftCollider.motorTorque = 0;
+    			frontRightCollider.motorTorque = 0;
+          		rearLeftCollider.motorTorque = 0;
+    			rearRightCollider.motorTorque = 0;
+    		}
+      	}
+    }
+
+	void ThrottleOff(){
+      	frontLeftCollider.motorTorque = 0;
+      	frontRightCollider.motorTorque = 0;
+      	rearLeftCollider.motorTorque = 0;
+      	rearRightCollider.motorTorque = 0;
+    }
+
+	void DecelerateCar(){
+      	if(throttleAxis != 0f){
+        	if(throttleAxis > 0f){
+          		throttleAxis = throttleAxis - (Time.deltaTime * 10f);
+        	}else if(throttleAxis < 0f){
+            	throttleAxis = throttleAxis + (Time.deltaTime * 10f);
+        	}
+        	if(Mathf.Abs(throttleAxis) < 0.15f){
+          		throttleAxis = 0f;
+        	}
+      	}
+      	rb.velocity = rb.velocity * (1f / (1f + (0.025f * decelerationMultiplier)));
+      	frontLeftCollider.motorTorque = 0;
+      	frontRightCollider.motorTorque = 0;
+      	rearLeftCollider.motorTorque = 0;
+      	rearRightCollider.motorTorque = 0;
+      	if(rb.velocity.magnitude < 0.25f){
+        	rb.velocity = Vector3.zero;
+        	CancelInvoke("DecelerateCar");
+      	}
+    }
+
+	void Brakes(){
+      	frontLeftCollider.brakeTorque = brakeForce;
+      	frontRightCollider.brakeTorque = brakeForce;
+      	rearLeftCollider.brakeTorque = brakeForce;
+      	rearRightCollider.brakeTorque = brakeForce;
     }
 }
