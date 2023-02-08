20a071db20965126e56c974d80f7152681d75505
deadlife1499 <96661238+deadlife1499@users.noreply.github.com>
2023-02-08 07:40:04 -0600

    hryhrt

--- Crash Test Dummy/Assets/Scripts/EnterVehicle.cs
+++ Crash Test Dummy/Assets/Scripts/EnterVehicle.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class EnterVehicle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    GameObject[] vehicleObjs;
+    GameObject player;
+    bool inCar;
+    GameObject currentCar;
+    public Camera camera;
+    public GameObject playerMesh;
+    public BeanMovement playerScript;
+    BasicVehControls carControls;
+    Camera carCamera;
+    public Transform playerExitLoc;
+
+    void Start() {
+        player = gameObject;
+        inCar = false;
+        vehicleObjs = GameObject.FindGameObjectsWithTag("DrivableCar");
+        foreach(GameObject obj in vehicleObjs) {
+            obj.GetComponent<BasicVehControls>().enabled = false;
+            obj.GetComponent<Camera>().enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(!inCar) {
+            foreach(GameObject obj in vehicleObjs) {
+                //if(CollisionDetection.IsTouching(obj, player) && Input.GetKey(KeyCode.E)) {
+                if(Input.GetKey(KeyCode.E)) {
+                    inCar = true;
+                    carControls = obj.GetComponent<BasicVehControls>();
+                    currentCar = obj;
+                    carCamera = obj.GetComponent<Camera>();
+                    EnterCar(obj);
+                    return;
+                }
+            }
+        }
+        else if(Input.GetKey(KeyCode.E)) {
+            ExitCar();
+        }
+    }
+
+    void EnterCar(GameObject car) {
+        camera.enabled = false;
+        playerMesh.SetActive(false);
+        playerScript.enabled = false;
+
+        carControls.enabled = true;
+        carCamera.enabled = true;
+    }
+
+    void ExitCar() {
+        carControls.enabled = false;
+        carCamera.enabled = false;
+
+        transform.position = playerExitLoc.position;
+
+        camera.enabled = true;
+        playerMesh.SetActive(true);
+        playerScript.enabled = true;
     }
 }
